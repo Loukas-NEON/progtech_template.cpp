00c5a53abdb8a4defd302c04eca2703f447567c2
LoukasNEON <loukasprofessional@gmail.com>
2020-08-06 13:39:28 +0000

    char* buf

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
-// cpp template with debug symbols compilation. git ignores main executable
+// adapted from http://www.cs.toronto.edu/~krueger/csc209h/tut/crash.c
 
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(){
+char* buf = (char*) malloc(10 * sizeof(char));
 
-string greeting = "Hello Progtech 2020!";
-  
-cout << greeting << endl;
+int sum_to_n(int num)
+{
+    int i,sum=0;
+    for(i=1;i<=num;i++)
+        sum+=i;
+    return sum;
+}
+
+void printSum()
+{
+    char line[10];
+    printf("enter a number:\n");
+    fgets(line, 10, stdin);
+    strtok(line, "\n"); 
+    sprintf(buf,"sum=%d",sum_to_n(atoi(line)));
+    printf("%s\n",buf);
+}
+
+int main(void)
+{
+    printSum();
+    return 0;
 }
